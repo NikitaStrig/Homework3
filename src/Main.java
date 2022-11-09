bc5e9ac55819c8bd3c70c6c6164e28c679d1653c
nikitastrigunkov <demonkurt1995@gmail.com>
2022-11-09 12:39:01 +0300

    HW3 Create.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        int a = 3;
+        byte b = 13;
+        short c = 23;
+        long d = 33;
+        float f = 43;
+        double g = 53.3333333;
+        System.out.println("Значение переменной a с типом int равно " +a);
+        System.out.println("Значение переменной b с типом byte равно " +b);
+        System.out.println("Значение переменной c с типом short равно " +c);
+        System.out.println("Значение переменной d с типом long равно " +d);
+        System.out.println("Значение переменной f с типом float равно " +f);
+        System.out.println("Значение переменной g с типом double равно " +g);
+
+    }
+}
